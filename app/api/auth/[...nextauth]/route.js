8e453877f46cbbeccf6437686e8589fe84a8c923
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-10-09 00:14:35 +0300

    send post request with details of logged in user

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,5 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 
@@ -10,8 +12,38 @@ const authOptions = {
     ],
     callbacks: {
         async signIn({ user, account }) {
-            console.log("User: ", user);
-            console.log("Account: ", account);
+            // console.log("User: ", user);
+            // console.log("Account: ", account);
+
+            if(account.provider === 'gooogle'){
+                const {name, email} = user;
+                try{
+                    await connectMongoDB();
+                    const userExists = await User.findOne({email});
+
+                    if(!userExists){
+                        const res = await fetch("http://localhost:3000/api/user", {
+                            method: "POST",
+                            headers: {
+                              "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                              name,
+                              email,
+                            }),
+                          });
+    
+                        if(res.ok){
+                            console.log('res');
+                            console.log(res);
+                            return user;
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+
             return user;
         }
     }
